ef15a07d6d687f562d036a0d7858d9271ec3f9f1
kevin <bqopdmail@gmail.com>
2025-05-21 22:41:54 -0500

    Cleaned up the project: I deleted previous code from tutorials. All code is now original. First clean version.

--- lib/pages/html_file_picker.dart
+++
@@ -1,2 +0,0 @@
-export 'html_file_picker_stub.dart'
-if (dart.library.html) 'html_file_picker_web.dart';
--- lib/pages/html_file_picker_stub.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:typed_data'; // Import for Uint8List
-
-Uint8List? getFileFromFilePicker() {
-  throw UnsupportedError('This function is only available on the web');
-}
