904ea3794f2e9b0cdeddf454e426d6a8fde23cd3
kevin <bqopdmail@gmail.com>
2026-05-12 23:13:27 -0500

    Steps 1-5 complete.

--- apps/bqopd_web/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:jaspr/client.dart';
-import 'app.dart';
-
-void main() {
-  // Removed Firebase.initializeApp() which requires Flutter engine
-  runApp(App());
-}
---
+++ apps/bqopd_web/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:jaspr/jaspr.dart';
+import 'package:bqopd_web/app.dart';
+
+void main() {
+  // This file MUST be in the web/ directory to compile to main.dart.js
+  runApp(App());
+}
